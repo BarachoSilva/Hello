ffc124fdcefcafa760a61519a50918e89104aa2d
alex_ <alex_rodrigues_11@hotmail.com>
2023-05-13 21:50:28 -0300

    new changes

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String name = "alex";
-        System.out.println("Hello, " + name);
+        String name = "Alex";
+        String sobre = "Silva";
+        System.out.println("Hello, " + name + sobre);
     }
 }
